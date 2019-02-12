dc2f2f94a5448f49e95ce09162619ff8a916df1d
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-02-12 11:34:32 -0500

    Completed exercises using foreach and while loops

--- ArraysAndLists/ArraysAndLists/Program.cs
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -10,27 +10,23 @@ namespace ArraysAndLists
     {
         static void Main(string[] args)
         {
-            Exercise1();
+            //Exercise1();
+            //Exercise2();
+            //Exercise3();
+            Exercise4();
             Console.ReadLine();
         }
 
         /*
-        1- When you post a message on Facebook, depending on the number of people who like your post, 
-        Facebook displays different information.
-        If no one likes your post, it doesn't display anything.
-        If only one person likes your post, it displays: [Friend's Name] likes your post.
-        If two people like your post, it displays: [Friend 1] and [Friend 2] like your post.
-        If more than two people like your post, it displays: [Friend 1], [Friend 2] and [Number of Other People] 
-        others like your post.
         Write a program and continuously ask the user to enter different names, 
         until the user presses Enter (without supplying a name).
         Depending on the number of names provided, display a message based on the above pattern.
         */
         public static void Exercise1()
         {
-            var likes = new List<string>(); 
+            var likes = new List<string>();
 
-            while(true)
+            while (true)
             {
                 Console.WriteLine("Enter a name, or press Enter without a name: ");
                 var input = Console.ReadLine();
@@ -48,24 +44,88 @@ namespace ArraysAndLists
                 Console.WriteLine();
         }
 
+        /*
+        2- Write a program and ask the user to enter their name. Use an array to reverse the name and 
+        then store the result in a new string. Display the reversed name on the console.
+        */
+        public static void Exercise2()
+        {
+            Console.WriteLine("Enter your name: ");
+            var name = Console.ReadLine();
+            var nameArray = name.ToCharArray(0, name.Length);
+            Array.Reverse(nameArray);
+
+            foreach (char c in nameArray)
+                Console.WriteLine(c);
+
+        }
 
 
         /*
-        2- Write a program and ask the user to enter their name. Use an array to reverse the name and then store the result in a new string. Display the reversed name on the console.
+        3- Write a program and ask the user to enter 5 numbers. If a number has been previously entered, 
+        display an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers, 
+        sort them and display the result on the console.
         */
+        public static void Exercise3()
+        {
+
+            var numbers = new List<int>();
+
+            while (numbers.Count < 5)
+            {
+                Console.WriteLine("Enter a number: ");
+                int newNum = Convert.ToInt32(Console.ReadLine());
+
+                if (numbers.Contains(newNum))
+                {
+                    Console.WriteLine("Oops! You already entered {0}.  Try again: ", newNum);
+                    continue;
+                }
+
+                numbers.Add(newNum);
+
+            }
+            numbers.Sort();
+            foreach (int number in numbers)
+                Console.WriteLine(number);
+        }
 
 
         /*
-        3- Write a program and ask the user to enter 5 numbers. If a number has been previously entered, display an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers, sort them and display the result on the console.
+        4- Write a program and ask the user to continuously enter a number or type "Quit" to exit. 
+        The list of numbers may include duplicates. Display the unique numbers that the user has entered.
         */
+        
+        public static void Exercise4()
+        {
+
+            var numbers = new List<int>();
+            
+            while(true)
+            {
+                Console.WriteLine("Enter a number, or type \"Quit\": ");
+                var input = Console.ReadLine();
+
+                if (input == "Quit" || input == "quit")
+                    break;
+
+                numbers.Add(Convert.ToInt32(input));
+            }
+
+            var uniqueNums = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (!uniqueNums.Contains(number))
+                    uniqueNums.Add(number);
+            }
+            Console.WriteLine("The unique numbers are: ");
+            foreach (var number in uniqueNums)
+                Console.WriteLine(number);
+             
+        }
+ 
 
 
-
-        /*
-        4- Write a program and ask the user to continuously enter a number or type "Quit" to exit. The list of numbers may include duplicates. Display the unique numbers that the user has entered.
-        */
-
-
-    
     }
 }
+
