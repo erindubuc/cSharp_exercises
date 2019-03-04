5b66a9111598dc2440d75d4e4d477cbfcc081ab2
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-03-04 12:13:45 -0500

    Returns number of times for bouncing ball to bounce past window

---
+++ BouncingBall/BouncingBall/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BouncingBall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            double height = Convert.ToDouble(Console.ReadLine());
+            double bounceRate = Convert.ToDouble(Console.ReadLine());
+            double windowHeight = Convert.ToDouble(Console.ReadLine());
+
+            int windowBounces = BallBounces(height, bounceRate, windowHeight);
+            Console.WriteLine(windowBounces);
+            Console.ReadLine();
+
+        }
+
+        // how many times will someone at a window see a ball bounce from height 
+        static int BallBounces(double height, double bounceRate, double windowHeight)
+        {
+            // height is > 0, greater than window
+            if (height < 0 || height < windowHeight)
+                return -1;
+
+            //bounce can't be less than 0 or greater than 1
+            if (bounceRate < 0 || bounceRate > 1)
+                return -1;
+
+            double totalBounces = bounceRate * height; 
+            return Convert.ToInt32(totalBounces * windowHeight);
+
+        }
+    }
+}
