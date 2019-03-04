e9f38ca4aa4a9cd532437f2613858282df80e09c
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-03-04 14:55:18 -0500

    Changed BallBounces function to included bounceHeight

--- BouncingBall/BouncingBall/Program.cs
+++ BouncingBall/BouncingBall/Program.cs
@@ -25,17 +25,26 @@ namespace BouncingBall
         // how many times will someone at a window see a ball bounce from height 
         static int BallBounces(double height, double bounceRate, double windowHeight)
         {
+            // How many times will the mother see the ball pass in 
+            // front of her window (including when it's falling and bouncing?)
+            // set to 1 for the "fall" 
+            int windowBounces = 1;
+
             // height is > 0, greater than window
-            if (height < 0 || height < windowHeight)
+            if (height < 0 || height <= windowHeight)
                 return -1;
 
             //bounce can't be less than 0 or greater than 1
             if (bounceRate < 0 || bounceRate > 1)
                 return -1;
 
-            double totalBounces = bounceRate * height; 
-            return Convert.ToInt32(totalBounces * windowHeight);
-
+            double bounceHeight = bounceRate * height;
+            while (bounceHeight > windowHeight)
+            {    
+                bounceHeight *= bounceRate;
+                windowBounces +=2;
+            }
+            return windowBounces;
         }
     }
 }
