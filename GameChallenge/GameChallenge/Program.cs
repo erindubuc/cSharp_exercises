efc8979fd35cff2cb7c9e0470cf4b96093d0187b
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-03-08 09:47:16 -0500

    Created func to draw a game board of specified width and height

---
+++ GameChallenge/GameChallenge/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameChallenge
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var g = new Game();
+            g.DrawBoard(6, 6);
+        
+            Console.ReadLine();
+        }
+    }
+}
