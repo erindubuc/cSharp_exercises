d8d254482dc19757aac0a82629f27c6a7722042a
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-04-08 21:48:10 -0400

    Completed method to return the area of an n-interesting polygon

---
+++ ShapeArea/ShapeArea/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ShapeArea
+{
+    public class Program
+    {
+        /*  Below we will define an n-interesting polygon. 
+            Your task is to find the area of a polygon for a given n.
+            A 1-interesting polygon is just a square with a side of 
+            length 1. An n-interesting polygon is obtained by taking 
+            the n - 1-interesting polygon and appending 1-interesting 
+            polygons to its rim, side by side. You can see the 1-, 2-, 
+            3- and 4-interesting polygons in the picture below.
+        */
+        public static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(ShapeArea(n));
+            
+        }
+        public static int ShapeArea(int n)
+        {
+
+            return n * n + ((n - 1) * (n - 1));
+        }
+    }
+}
