6ff25c6608959c668fd9639b631e56ba26a681ce
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-04-09 14:22:24 -0400

    Game applies a move to the board and displays a game piece

---
+++ ConnectFourVersion4/ConnectFourVersion4/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectFourVersion4
+{
+    public class Game
+    {
+        public char gamePiece;
+
+        public char[,] GetPositionOfGameMove(int height, int width)
+        {
+            char[,] movePosition = new char[height, width];
+            return movePosition;
+        }
+
+        public void DropPiece(char[,] movePosition)
+        {
+            Console.WriteLine("Please enter a number between 1 and 7: ");
+            int dropColumn = Convert.ToInt32(Console.ReadLine()) - 1;
+            int rowHeight = movePosition.GetLength(0) - 1;
+
+            gamePiece = (movePosition[rowHeight, dropColumn] != 'X') ? 'X' : 'O';
+        }
+    }
+}
---
+++ ConnectFourVersion4/ConnectFourVersion4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConnectFourVersion4
+{
+    class Board
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter height of board between 2 and 30: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter width of board between 2 and 30");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+
+            if (height < 2 || height > 30)
+            {
+                Console.WriteLine("Please enter a valid height");
+            }
+            if (width < 2 || width > 30)
+            {
+                Console.WriteLine("Please enter a valid width");
+            }
+
+            GameBoard board = new GameBoard(height, width);
+            board.DrawBoard(height, width);
+
+            Game myGame = new Game();
+            char[,] gameMove = myGame.GetPositionOfGameMove(height, width);
+
+            myGame.DropPiece(gameMove);
+            board.DisplayBoardAfterMove(myGame.gamePiece);
+
+        }
+
+
+
+
+
+
+
+    }
+}
